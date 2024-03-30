30acd7467ba0ed28da9eb5ab475e47469600c73d
RomanaKarasenko <143837231+RomanaKarasenko@users.noreply.github.com>
2024-03-30 21:37:56 +0200

    Contact-css

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,19 +1,24 @@
 import { MdPhoneIphone } from "react-icons/md";
 import { BiSolidContact } from "react-icons/bi";
+import styles from "./Contact.module.css";
 
 const Contact = ({ id, username, number, onDelete }) => {
   return (
-    <li key={id}>
-        <div>
-      <BiSolidContact />
-      <p>Name: {username}</p>
-      </div>
-      <div>
-      <MdPhoneIphone />
-      <p>Number: {number}</p>
-      </div>
-      <button onClick={() => onDelete(id)}>Delete</button>
-    </li>
+    <div className={styles.contactStyle}>
+      <li key={id}>
+        <div className={styles.textContainer}>
+          <BiSolidContact className={styles.svg} />
+          <p className={styles.textStyle}>Name: {username}</p>
+        </div>
+        <div className={styles.textContainer}>
+          <MdPhoneIphone className={styles.svg} />
+          <p className={styles.textStyle}>Number: {number}</p>
+        </div>
+      </li>
+      <button className={styles.deleteButton} onClick={() => onDelete(id)}>
+        Delete
+      </button>     
+    </div>
   );
 };
 
